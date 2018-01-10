8a8c0f5a72cdacec3ddcbaa890322e1f83af9995
Chris Gullickson <cgullickson5@gmail.com>
2018-01-10 16:50:30 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -1,14 +1,15 @@
+require 'pry'
+
 class Application
 
   def call(env)
     resp = Rack::Response.new
 
     if Time.now < 12
+      binding.pry
       resp.write "Good Morning!"
     else
       resp.write "Good Afternoon!"
     end
   end
 end
-
-    
